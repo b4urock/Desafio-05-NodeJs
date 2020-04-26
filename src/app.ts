3607cd18b21b3903d14a9abaf86d0ee1c5519d2f
Pablo Pereira <b4urock@yahoo.com>
2020-04-26 17:44:55 -0300

    :sparkles: final version

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import * as bodyParser from 'express';
 import routes from './routes';
 
 const app = express();
 app.use(express.json());
+app.use(bodyParser.json());
 app.use(routes);
 
 export default app;
